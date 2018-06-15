8756f3aa5c07ed87de51141535de5c1c73ebe1a4
akashjwu14 <40304906+akashjwu14@users.noreply.github.com>
2018-06-15 17:06:06 -0500

    Add files via upload

---
+++ JavaLevel1.java
@@ -0,0 +1,32 @@
+
+public class JavaLevel1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("**********Welcome to Java Level 1*************");
+		
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		
+		int x = a+b+c;
+		
+		System.out.println("Sum of x:" + x);
+		
+		System.out.println("********************************************");
+		
+		int d = 11;
+		float e = d;
+		float f = 11.5f;
+		int g=(int)f;
+		
+		System.out.println(f);
+		System.out.println(g);
+		
+		System.out.println(d);
+		System.out.println(e);
+		
+	}
+
+}
